32190a56e2e5c2b14dbdb3bf02d6ee1abc3a0183
Tyson-Granger <91722237+Tyson-Granger@users.noreply.github.com>
2022-11-29 20:54:38 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+var first_name = "Vihaan"
+var last_name = "Chawla"
+var full_name = first_name.concat(last_name) 
+console.log(full_name)
+var a = 1
+var b = 7
+var add = a + b
+console.log(add)
+var subtraction = a - b
+console.log(subtraction)
